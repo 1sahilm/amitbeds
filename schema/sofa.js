fec6646fc02455034b535c328daf9a94156f7322
1sashilm <msahil76640@gmail.com>
2021-10-06 10:29:33 +0530

    first commit

---
+++ schema/sofa.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const sofaSchema = mongoose.Schema({
+  product_name: { type: String, required: false },
+  description: { type: String, required: false },
+  size: { type: String, required: false },
+  price: { type: Number, required: false },
+  images: { type: Array, required: false },
+  timestamp: String,
+});
+
+module.exports = mongoose?.models?.sofa || mongoose.model('sofa', sofaSchema);
